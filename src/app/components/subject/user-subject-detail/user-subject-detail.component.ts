1266081ebbdf98944d3d101f8d72e18a004f154a
LCA <lca@gen7.com>
2018-07-23 23:21:14 +0530

    Filter in GetAllUser. Users from the list were seleted using the filter

--- src/app/components/subject/user-subject-detail/user-subject-detail.component.ts
+++ src/app/components/subject/user-subject-detail/user-subject-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../../services/user.service";
+import { UserInfo } from '../../../model/UserInfo';
+import { map, filter } from '../../../../../node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-user-subject-detail',
@@ -9,18 +11,42 @@ import { UserService } from "../../../services/user.service";
 export class UserSubjectDetailComponent implements OnInit {
 
   firstName : string;
+  roleId : number;
+  userInfo : UserInfo;
+  userList : UserInfo[];
+  userId : number;
 
   constructor(private userService : UserService) { }
 
   ngOnInit() {
     this.userService.userInfoSubject.
-      subscribe(data => this.firstName = data.FirstName);
+      subscribe(data => this.userInfo = data);
+      
+    this.userService.userInfoSubject.
+      subscribe(data =>{
+          this.firstName = data.FirstName;
+          this.roleId = data.RoleId;
+        }
+      );
 
     this.userService.getUserUsingSubject();
+
+    this.GetAllUsers()
+  }
+  GetUserUsingSubject(){
+    this.userService.getUserUsingSubject(); 
   }
 
   UpdateUserUserSubject(){
     this.userService.updateUserUsingSubject();
   }
 
+  GetAllUsers(){
+    this.userService.getAllUsers(this.userId)
+    .subscribe(
+      data => {
+      this.userList = data
+    }
+    );
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import { observable, Observable, Subject } from 'rxjs';
 import { UserInfo } from '../model/UserInfo';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
+// import * as Rx from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  // public userInfoSubject = new Rx.ReplaySubject<UserInfo>(2);
   public userInfoSubject = new Subject<UserInfo>();
 
   constructor(private http : HttpClient) { }
 
   getUser() : Observable<UserInfo>{
     const url = 'http://localhost/api/user/getuser';
-    return this.http.get<UserInfo>(url);
+    return this.http.get<UserInfo>(url)
+    .pipe(
+      filter(serviceData => serviceData.RoleId === 1)
+    );
   }
 
   updateUser() : Observable<UserInfo>{
     const url = 'http://localhost/api/user/updateuser';
-    return this.http.put<UserInfo>(url, 'something')
+
+    let userInfo : UserInfo = {
+      FirstName:'FName1Updated',
+      LastName:'LName1Updated',
+      LoginId:'FL1',
+      RoleId:1,
+      UserId:1
+    };
+
+    return this.http.put<UserInfo>(url, userInfo)
   }
 
   getUserUsingSubject(){
@@ -32,20 +46,44 @@ export class UserService {
 
   updateUserUsingSubject(){
     const url = 'http://localhost/api/user/updateuser';
-    this.http.put<UserInfo>(url, 'something').
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+
+    let userInfo : UserInfo = {
+      FirstName:'FName1Updated',
+      LastName:'LName1Updated',
+      LoginId:'FL1',
+      RoleId:1,
+      UserId:1
+    }
+
+    this.http.put<UserInfo>(url, userInfo, httpOptions).
       pipe(map((returnData:UserInfo)=>
       {
         return{
           FirstName:'Name is updated by MAP -- ' + returnData.FirstName,
           LastName:returnData.LastName,
           LoginId:returnData.LoginId,
-          RoleId:returnData.RoleId,
-          UserId:returnData.UserId,
+          RoleId:returnData.RoleId * 2,
+          UserId:returnData.UserId
         }
       }
       )).
       subscribe(
       serviceData => this.userInfoSubject.next(serviceData)
     );
+  }
+
+  getAllUsers(userId : number) : Observable<Array<UserInfo>>{
+    const url = 'http://localhost/api/user/getallusers';
+    return this.http.get<Array<UserInfo>>(url)
+      .pipe(
+        map(serviceData => serviceData.filter(singleServiceData => singleServiceData.UserId > userId)
+      )
+    )
   }
 }
